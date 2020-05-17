5b587362e0829a0481c19d9987591b890fd2fa69
mial0 <mialozo93@gmail.com>
2020-05-17 13:38:36 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+mood=input("Hello, how do you feel?").lower()
+if mood=="happy":
+    print("It is great to see you happy!")
+elif mood=="nervous":
+    print("Take a deep breath 3 times.")
+elif mood=="sad":
+    print("Cheer up!")
+elif mood=="excited":
+    print("I am glad that you are excited!")
+elif mood=="relaxed":
+    print("It is great to see you feeling like that!")
+else:
+    print("I don't recognize this mood.")
